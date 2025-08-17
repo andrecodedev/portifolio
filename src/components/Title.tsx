6725fb6a344879d8a602676fa7ae570585a62547
andre <salesdias1207@gmail.com>
2025-08-17 10:07:56 -0300

    title corte

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -24,37 +24,37 @@ export default function Title({
     <>
     <div className="flex flex-1 flex-col justify-center items-center text-center px-4 py-6 sm:px-8 lg:px-16">
       {title_1 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_1} {t('page_titles.about')}
         </h2>
       )}
 
       {title_2 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_2} {t('page_titles.aboutExpPro')}
         </h2>
       )}
 
       {title_3 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_3} {t('page_titles.education')}
         </h2>
       )}
 
       {title_4 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_4} {t('page_titles.skills')}
         </h2>
       )}
             
       {title_5 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_5} {t('page_titles.projects')}
         </h2>
       )}
 
       {title_6 && (
-        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold leading-tight bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
+        <h2 className="text-[3rem] mb-1 mt-1 font-medium text-start font-extrabold bg-gradient-to-r from-[var(--primary-linear-gradient)] to-[var(--text-gray-linear-gradient)] bg-clip-text text-transparent relative inline-block select-none">
           {title_6} {t('page_titles.contact')}
         </h2>
       )}
